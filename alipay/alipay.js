07df62b1e5d2eb86152751809b70f249bb5b22ae
Canjiang Lu <craftsfish@163.com>
2017-08-13 14:38:44 +0800

    get total record ok

--- alipay/alipay.js
+++ alipay/alipay.js
@@ -1,9 +1,20 @@
 log("alipay");
-items = jQuery("#tradeRecordsIndex tbody tr")
-log(items.length);
 
 var i = 0;
 for (i=0; i<20; i++) {
   var selector = "#tradeRecordsIndex tbody tr:eq(" + i.toString() + ") .time-d";
   log(jQuery(selector)[0].innerHTML);
 }
+
+function getTotal() {
+  items = jQuery("#tradeRecordsIndex tbody tr")
+  return Promise.resolve(items.length);
+}
+
+function onMsg(m)
+{
+  if (m.action == "queryTotal") {
+    return getTotal();
+  }
+}
+browser.runtime.onMessage.addListener(onMsg);
--- alipay/bg.js
+++ alipay/bg.js
@@ -10,6 +10,7 @@ function startProcessing() {
     "https://consumeprod.alipay.com/record/advanced.htm"
   ]})
   .then(onRecordFound)
+  .then(onTotalReceived)
   .catch(onError);
 }
 
@@ -17,11 +18,22 @@ function onRecordFound(tabs) {
   if (tabs.length == 1) {
     tabid_record = tabs[0].id;
     log(tabid_record);
+    return sndMsg(tabid_record, "queryTotal");
   } else {
     throw new Error("Cannot find tab of Alipay Record!");
   }
 }
 
+function onTotalReceived(m) {
+  log(m);
+}
+
 function onError(error) {
   err(`Error: ${error}`);
+}
+
+/* interaction with content scripts */
+function sndMsg(id, a, d) {
+  log(`send message to id : action is ${a}, data is ${d}`);
+  return browser.tabs.sendMessage(id, {action: a, data: d});
 }
