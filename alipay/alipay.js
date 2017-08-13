4c00ce29c6b669c7565b3099fee1b45904454d20
Canjiang Lu <craftsfish@163.com>
2017-08-13 10:53:13 +0800

    [alipay]parse page info

--- alipay/alipay.js
+++ alipay/alipay.js
@@ -1,1 +1,9 @@
 log("alipay");
+items = jQuery("#tradeRecordsIndex tbody tr")
+log(items.length);
+
+var i = 0;
+for (i=0; i<20; i++) {
+  var selector = "#tradeRecordsIndex tbody tr:eq(" + i.toString() + ") .time-d";
+  log(jQuery(selector)[0].innerHTML);
+}
