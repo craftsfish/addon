9febd4504c5f510ec0f0bdc7a97f1a42984ce443
Canjiang Lu <craftsfish@163.com>
2018-10-29 20:13:27 +0800

    trivial

--- weikequan.delivery/background-script.js
+++ weikequan.delivery/background-script.js
@@ -10,11 +10,29 @@ var t = new Date();
 log(t.toDateString());
 log(t.toTimeString());
 
+function handleOrder() {
+  cur_order++;
+  if (cur_order >= map.length) {
+    return
+  }
+  log(`handling: ${cur_order}/${map.length}`);
+  browser.tabs.create({url:"https://porder.shop.jd.com/order/singleOut/"+map[cur_order][0]+"?selectedDelivery=2170"});
+}
+
+function createDelayPromise(timeout) {
+  delayPromise.expireAt = new Date().getTime() + timeout;
+  return new Promise((resolve, reject)=>{delayPromise.resolve = resolve; delayPromise.reject = reject});
+}
+
+function onJdOrderDone() {
+  browser.tabs.sendMessage(tabId, map[cur_order][1])
+  .then(handleOrder);
+}
+
 /* browser action handling */
 function handleClick() {
-    cur_order = 0;
-    log(`handling: ${cur_order}/${map.length}`);
-    browser.tabs.create({url:"https://porder.shop.jd.com/order/singleOut/"+map[cur_order][0]+"?selectedDelivery=2170"});
+  cur_order = -1;
+  handleOrder()
 }
 browser.browserAction.onClicked.addListener(handleClick);
 
@@ -22,7 +40,7 @@ function onTabsUpdated(tabId, changeInfo, tabInfo) {
   if (changeInfo.status == "complete") { /* loading complete */
     if (tabInfo.url.match(reg_jd_deliver) != null) {
       log("正在处理订单: " + map[cur_order][0] + ", 快递单号: " + map[cur_order][1]);
-      browser.tabs.sendMessage(tabId, map[cur_order][1]);
+      createDelayPromise(5*1000).then(onJdOrderDone)
     }
   }
 }
--- weikequan.delivery/content-script-jd.js
+++ weikequan.delivery/content-script-jd.js
@@ -2,11 +2,24 @@ console.log("Content script of JD is running");
 
 function delivery(id)
 {
-  $("div.ivu-select-selection")[0].click()
-  $("ul.ivu-select-dropdown-list li")[2].click()
-  $("div.out-delivery-input.ivu-input-wrapper.ivu-input-type input")[0].value = id;
-  //$("button.btn-so.ivu-btn.ivu-btn-primary")[0].click();
-  return Promise.resolve("ok");
+  console.log("11111111111111111");
+  console.log($("div.ivu-select-selection"))
+  if ($("div.ivu-select-selection").length == 1) {
+    $("div.ivu-select-selection")[0].click()
+    $("ul.ivu-select-dropdown-list li").each(function(){
+      if ($(this).text() == '中通速递') {
+        $(this).click()
+        console.log("设置物流公司为: 中通速递");
+        return false
+      }
+    });
+    $("div.out-delivery-input.ivu-input-wrapper.ivu-input-type input")[0].value = id;
+    console.log("设置物流单号为: " + id);
+    //$("button.btn-so.ivu-btn.ivu-btn-primary")[0].click();
+    return Promise.resolve("ok");
+  }
+  console.log("22222222222");
+  return Promise.reject(new Error("设置物流信息失败"));
 }
 
 function onMsg(m)
