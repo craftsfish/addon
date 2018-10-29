567fe80eab3e98b9179ddfe2c0a53d872b8331cf
Canjiang Lu <craftsfish@163.com>
2018-10-29 18:29:07 +0800

    trivial

--- weikequan.delivery/background-script.js
+++ weikequan.delivery/background-script.js
@@ -4,24 +4,24 @@ var map = [
 	[81500013740, 55129],
 	[81162947187, 55130],
 ];
-var reg_jd_deliver = new RegExp("https:\/\/porder.shop.jd.com\/order\/singleOut\/.*\?selectedDelivery=2170$")
+var cur_order = 0;
+var reg_jd_deliver = new RegExp("https:\/\/porder.shop.jd.com\/order\/singleOut\/.*\?selectedDelivery=2170$");
 var t = new Date();
 log(t.toDateString());
 log(t.toTimeString());
 
 /* browser action handling */
 function handleClick() {
-  for (var i=0; i<map.length; i++) {
-    log(`handling: ${i}/${map.length}`);
-    browser.tabs.create({url:"https://porder.shop.jd.com/order/singleOut/"+map[i][0]+"?selectedDelivery=2170"});
-  }
+    cur_order = 0;
+    log(`handling: ${cur_order}/${map.length}`);
+    browser.tabs.create({url:"https://porder.shop.jd.com/order/singleOut/"+map[cur_order][0]+"?selectedDelivery=2170"});
 }
 browser.browserAction.onClicked.addListener(handleClick);
 
 function onTabsUpdated(tabId, changeInfo, tabInfo) {
   if (changeInfo.status == "complete") { /* loading complete */
     if (tabInfo.url.match(reg_jd_deliver) != null) {
-        log("================================" + tabInfo.url);
+        log("================================" + tabInfo.url + cur_order);
     }
   }
 }
