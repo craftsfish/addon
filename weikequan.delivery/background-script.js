ac2cc929f41ec7e77d9056789108d1b372e76d77
Canjiang Lu <craftsfish@163.com>
2018-10-30 10:03:05 +0800

    trivial

--- weikequan.delivery/background-script.js
+++ weikequan.delivery/background-script.js
@@ -1,7 +1,7 @@
 log("WeiKeQuan background is running");
 
 var map = [
-	[81500013740, 99999999],
+	[81506877675, 99999999],
 	[81162947187, 55130],
 ];
 var cur_order = 0;
@@ -26,11 +26,42 @@ function createDelayPromise(timeout) {
   return new Promise((resolve, reject)=>{delayPromise.resolve = resolve; delayPromise.reject = reject});
 }
 
-function onJdOrderDone() {
-  browser.tabs.sendMessage(tab, map[cur_order][1])
-  .then(handleOrder);
+function deliver() {
+  return sndMsg(tab, 'deliver', map[cur_order][1])
 }
 
+function setExpressId() {
+  return sndMsg(tab, 'setExpressId', map[cur_order][1])
+}
+
+function selectSupplier() {
+  return sndMsg(tab, 'selectSupplier', null)
+}
+
+function onSupplierCandidatesOpened() {
+  return createDelayPromise(5*1000);
+}
+
+function openSupplierCandidates() {
+  return sndMsg(tab, 'openSupplierCandidates', null)
+}
+
+function onTabsUpdated(tabId, changeInfo, tabInfo) {
+  if (changeInfo.status == "complete") { /* loading complete */
+    if (tabInfo.url.match(reg_jd_deliver) != null) {
+      log("正在处理订单: " + map[cur_order][0] + ", 快递单号: " + map[cur_order][1]);
+      tab = tabId
+      openSupplierCandidates()
+      .then(onSupplierCandidatesOpened)
+      .then(selectSupplier)
+      .then(setExpressId)
+      .then(deliver)
+      .then(handleOrder);
+    }
+  }
+}
+
+browser.tabs.onUpdated.addListener(onTabsUpdated);
 
 /*
  * periodic task
@@ -53,6 +84,11 @@ function handleAlarm(alarmInfo) {
 }
 browser.alarms.onAlarm.addListener(handleAlarm);
 
+/* interaction with content scripts */
+function sndMsg(id, a, d) {
+  log(`send message to ${id} : action is ${a}, data is ${d}`);
+  return browser.tabs.sendMessage(id, {action: a, data: d});
+}
 
 /* browser action handling */
 function handleClick() {
@@ -60,15 +96,3 @@ function handleClick() {
   handleOrder()
 }
 browser.browserAction.onClicked.addListener(handleClick);
-
-function onTabsUpdated(tabId, changeInfo, tabInfo) {
-  if (changeInfo.status == "complete") { /* loading complete */
-    if (tabInfo.url.match(reg_jd_deliver) != null) {
-      log("正在处理订单: " + map[cur_order][0] + ", 快递单号: " + map[cur_order][1]);
-      tab = tabId
-      createDelayPromise(5*1000).then(onJdOrderDone)
-    }
-  }
-}
-
-browser.tabs.onUpdated.addListener(onTabsUpdated);
--- weikequan.delivery/content-script-jd.js
+++ weikequan.delivery/content-script-jd.js
@@ -1,32 +1,50 @@
 console.log("Content script of JD is running");
 
-function delivery(id)
+function openSupplierCandidates(data)
 {
   if ($("div.ivu-select-selection").length == 1) {
     $("div.ivu-select-selection")[0].click()
-    var supplier;
-    $("ul.ivu-select-dropdown-list li").each(function(){
-      console.log($(this).text());
-      if ($(this).text() == '中通速递') {
-        supplier = $(this)
-        return false;
-      }
-    });
-    console.log(supplier.text());
-    supplier.click()
-    console.log("设置物流公司为: 中通速递");
-    $("div.out-delivery-input.ivu-input-wrapper.ivu-input-type input")[0].value = id;
-    console.log("设置物流单号为: " + id);
-    //$("button.btn-so.ivu-btn.ivu-btn-primary")[0].click();
     return Promise.resolve("ok");
   }
-  console.log("22222222222");
-  return Promise.reject(new Error("设置物流信息失败"));
+  return Promise.reject(new Error("打开物流供应商列表失败"));
+}
+
+function selectSupplier(data)
+{
+  var i
+  $("ul.ivu-select-dropdown-list li").each(function(){
+    if ($(this).text() == '中通速递') {
+      i = $(this)
+    }
+  });
+  i.click()
+  return Promise.resolve("ok");
+}
+
+function setExpressId(id)
+{
+  $("div.out-delivery-input.ivu-input-wrapper.ivu-input-type input")[0].value = id;
+  console.log("设置物流单号为: " + id);
+  return Promise.resolve("ok");
+}
+
+function deliver(data)
+{
+  //$("button.btn-so.ivu-btn.ivu-btn-primary")[0].click();
+  return Promise.reject(new Error("test"));
+  //return Promise.resolve("ok");
 }
 
 function onMsg(m)
 {
-  return delivery(m)
+  if (m.action == "openSupplierCandidates") {
+    return openSupplierCandidates(m.data);
+  } else if (m.action == "selectSupplier") {
+    return selectSupplier(m.data);
+  } else if (m.action == "setExpressId") {
+    return setMark(m.data);
+  } else if (m.action == "deliver") {
+    return deliver(m.data);
+  }
 }
-
 browser.runtime.onMessage.addListener(onMsg);
