8ce588b77422496715ddd579a630b031c41d9b2a
Canjiang Lu <craftsfish@163.com>
2018-12-07 20:52:45 +0800

    trivial

--- tm.candidate/content-script-tm.js
+++ tm.candidate/content-script-tm.js
@@ -14,33 +14,6 @@ function collect(data)
   console.log(r)
 }
 
-function collectElements(data)
-{
-  $("#q")[0].value = data
-  $("#J_SearchForm").submit()
-  return Promise.resolve("ok");
-  var elements = []
-  $("span.H").each(function(){
-    var hit = false
-    if ($(this).parents().filter("div.item.J_MouserOnverReq.item-ad").length > 0) {
-      return
-    }
-    if ($(this).parents().filter("div.tb-combobar.m-tips").length > 0) {
-      return
-    }
-    for (var i=0; i<elements.length; i++) {
-      if ($(this).text() == elements[i]) {
-        hit = true
-        break
-      }
-    }
-    if (!hit) {
-      elements[elements.length] = $(this).text()
-    }
-  });
-  return Promise.resolve(elements);
-}
-
 function onMsg(m)
 {
   if (m.action == "collect") {
