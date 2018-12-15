7c4ba6a6ff6599f8a038d73a36256d1435432e01
Canjiang Lu <craftsfish@163.com>
2018-12-15 22:23:03 +0800

    trivial

--- wkq/background-script.js
+++ wkq/background-script.js
@@ -1,22 +1,14 @@
 log("WeiKeQuan background is running");
 
-const ID_JD_DELIVER = 0;
-const ID_WKQ_DELIVER = 1;
-var map = [
-[81811853977,541797531991],
-[81816960114,541671869550],
-[81817247479,541797531913],
-[81816372722,541671869636],
-[82226976709,541674480406],
-[81809868409,541674480489],
-[82226062380,541673310017],
-[82224258498,541673310095],
-[82222918987,541673976028],
-[81800459323,541673976104],
-[82223274882,541671869712],
-];
-var cur_order = 0;
+const ID_WKQ_DELIVER = 0;
 var delayPromise = {expireAt: -1, resolve: undefined, reject: undefined};
+
+/*
+ * Tabs Updated Handling
+ */
+var Pages = [
+  {name:"wkq_deliver", regexp: new RegExp("https:\/\/qqq.wkquan2018.com\/Fine\/VTask")},
+];
 
 function createDelayPromise(timeout) {
   delayPromise.expireAt = new Date().getTime() + timeout;
@@ -25,20 +17,6 @@ function createDelayPromise(timeout) {
 
 function delay_1() {
   return createDelayPromise(1*1000);
-}
-
-function onWkqDeliveryError(error) {
-  err(error);
-  //browser.tabs.remove(Pages[ID_WKQ_DELIVER].tabId);
-  startJdDelivery();
-}
-
-function wkq_remove_tab() {
-  return browser.tabs.remove(Pages[ID_WKQ_DELIVER].tabId);
-}
-
-function wkq_select_supplier() {
-  return sndMsg(ID_WKQ_DELIVER, 'select', map[cur_order][1])
 }
 
 function wkq_get_order_info() {
@@ -59,69 +37,13 @@ function startWkqDelivery() {
     .then(wkq_init)
     .then(wkq_on_init)
     .then(wkq_get_order_info)
-//    .then(delay_1)
-//    .then(wkq_select_supplier)
-//    .then(delay_1)
-//    .then(startJdDelivery)
-//    .catch(onWkqDeliveryError);
 }
-
-function onJdDeliveryError(error) {
-  err(error);
-  //browser.tabs.remove(Pages[ID_JD_DELIVER].tabId);
-  startWkqDelivery();
-}
-
-function remove_jd_tab() {
-  return browser.tabs.remove(Pages[ID_JD_DELIVER].tabId);
-}
-
-function deliver() {
-  return sndMsg(ID_JD_DELIVER, 'deliver', null)
-}
-
-function setExpressId() {
-  return sndMsg(ID_JD_DELIVER, 'setExpressId', map[cur_order][1])
-}
-
-function selectSupplier() {
-  return sndMsg(ID_JD_DELIVER, 'selectSupplier', null)
-}
-
-function openSupplierCandidates() {
-  return sndMsg(ID_JD_DELIVER, 'openSupplierCandidates', null)
-}
-
-function startJdDelivery() {
-  cur_order++;
-  if (cur_order >= map.length) {
-    return
-  }
-  browser.tabs.create({active:false, url:"https://porder.shop.jd.com/order/singleOut/"+map[cur_order][0]+"?selectedDelivery=2170"});
-  return new Promise((resolve, reject) => {Pages[ID_JD_DELIVER].resolve = resolve;})
-    .then(delay_1)
-    .then(openSupplierCandidates)
-    .then(selectSupplier)
-    .then(delay_1)
-    .then(setExpressId)
-    .then(startWkqDelivery)
-    .catch(onJdDeliveryError);
-}
-
-/*
- * Tabs Updated Handling
- */
-var Pages = [
-  {name:"jd_deliver", regexp: new RegExp("https:\/\/porder.shop.jd.com\/order\/singleOut\/.*\?selectedDelivery=2170$")},
-  {name:"wkq_deliver", regexp: new RegExp("https:\/\/qqq.wkquan2018.com\/Fine\/VTask")},
-];
 
 function onTabsUpdated(tabId, changeInfo, tabInfo) {
   if (changeInfo.status != "complete") { /* loading complete */
     return
   }
 
-  log("loadcomplete")
   var i = 0;
   for (i=0; i<Pages.length; i++) {
     if (tabInfo.url.match(Pages[i].regexp) != null) {
@@ -165,7 +87,6 @@ function sndMsg(id, a, d) {
 
 /* browser action handling */
 function handleClick() {
-  cur_order = -1;
   startWkqDelivery()
 }
 browser.browserAction.onClicked.addListener(handleClick);
